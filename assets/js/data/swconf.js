f85beac3d86e74ffa0f399af867017ef9ed8b028
Andreas Aabrandt <andreas@emacs.io>
2025-11-04 22:46:27 +0100

    Update with post.

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,30 @@
-const swconf = { cacheName: 'chirpy-1762183167',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = {
+  
+    cacheName: 'chirpy-1762324335',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
